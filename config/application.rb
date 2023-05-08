0e596ebbf1e4e34fb9c12e070a146187c37b9404
jacquesmarques <jmarques1@gmail.com>
2023-05-08 18:36:17 -0300

    feat: setup compress on API

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module CoodeshChallenge
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use Rack::Deflater
   end
 end
